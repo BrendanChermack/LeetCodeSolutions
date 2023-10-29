fe2650151881805ea924a5b96b1d87ea6c7a429b
“BrendanChermack” <ChermackBrendan@gmail.com>
2023-10-29 16:29:17 -0500

    added solutions

---
+++ 1TwoSum.ts
@@ -0,0 +1,30 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+function twoSum(nums: number[], target: number): number[] {
+	let map = new Map<number, number>();
+	for (let i: number = 0; i < nums.length; i++) {
+		let diff: number = target - nums[i];
+		if (diff in map) {
+			return [map[diff], i];
+		}
+		map[nums[i]] = i;
+	}
+}
